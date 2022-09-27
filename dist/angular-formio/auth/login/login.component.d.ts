361908e7795bb175efd1e3779120160a5040e7be
Branislav <branislav.petkovski@htecgroup.com>
2022-09-27 15:45:50 +0200

    Build update

--- dist/angular-formio/auth/login/login.component.d.ts
+++
@@ -1,9 +0,0 @@
-import { FormioAuthService } from '../auth.service';
-import * as i0 from "@angular/core";
-export declare class FormioAuthLoginComponent {
-    service: FormioAuthService;
-    constructor(service: FormioAuthService);
-    static ɵfac: i0.ɵɵFactoryDeclaration<FormioAuthLoginComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<FormioAuthLoginComponent, "ng-component", never, {}, {}, never, never, false>;
-}
-//# sourceMappingURL=login.component.d.ts.map
--- dist/angular-formio/components/loader/formio.loader.component.d.ts
+++
@@ -1,7 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class FormioLoaderComponent {
-    isLoading: boolean;
-    static ɵfac: i0.ɵɵFactoryDeclaration<FormioLoaderComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<FormioLoaderComponent, "formio-loader", never, { "isLoading": "isLoading"; }, {}, never, never, false>;
-}
-//# sourceMappingURL=formio.loader.component.d.ts.map
